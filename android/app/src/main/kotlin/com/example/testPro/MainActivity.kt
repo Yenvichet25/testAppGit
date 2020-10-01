be669e0c13e0e8fd40aadea5834af40f74771b28
leyleygit <rulestarboy@gmail.com>
2020-10-01 15:57:36 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/testPro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testPro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        width: size.width,
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("I want to kill you")],
+        ),
+      ),
+    );
+  }
+}
